aa7ffb482eac94397e13c7c48f902f25d81fa44f
Welington Marquezini <welington_marquezini88@live.com>
2020-10-06 17:48:08 -0300

    Inclusao do pedidoDAO

--- src/Controller/PedidoController.java
+++ src/Controller/PedidoController.java
@@ -1,9 +1,20 @@
 package Controller;
 
+import DAO.PedidoDAO;
+import java.util.List;
+
 /**
  *
  * @author welingtonmarquezini
  */
 public class PedidoController {
+    PedidoDAO pedidoDAO;
+
+    public PedidoController() {
+        pedidoDAO = new PedidoDAO();
+    }
     
+    public void controleItens(String pesquisa,List<String> listaItens){
+        pedidoDAO.pesquisaItens(pesquisa, listaItens);
+    }
 }
