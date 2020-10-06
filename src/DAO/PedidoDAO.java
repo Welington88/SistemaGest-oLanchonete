b977fabb2828bf4ee04079a5d77298107f2dafd2
Welington Marquezini <welington_marquezini88@live.com>
2020-10-06 17:24:10 -0300

    builder

--- src/DAO/PedidoDAO.java
+++ src/DAO/PedidoDAO.java
@@ -1,10 +1,31 @@
 package DAO;
 
+import Utilitarios.Conexao;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+import javax.swing.ImageIcon;
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author welingtonmarquezini
  */
 public class PedidoDAO {
     
+    public void pesquisaItens(String pesquisa, List<String> listaDeItens){
+        try {
+            String SQLPesquisa = "SELECT * FROM `cardapio` WHERE `car_descricao` Like '%" + pesquisa + "%';";
+            PreparedStatement ps = Conexao.getConnection().prepareStatement(pesquisa);
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {                
+                listaDeItens.add(rs.getString("car_descricao"));
+            }
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, 
+              "Erro ao consultar Banco de Dados" + ex, "Erro", 0,new ImageIcon("imagens/ico_sair.png"));//mensagem de erro
+        }
+    }
     
 }
